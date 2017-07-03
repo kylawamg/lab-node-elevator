7fa715ba38b1d41f004558b026acaee3db58aa07
alejandro <alejandro.m.gomez.92@gmail.com>
2017-07-03 17:39:01 +0200

    iteration 3

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,55 @@
+/*jshint esversion:6*/
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = null;
+    this.intervalId = null;
+    this.waitingList = [];
+    this.passengers = [];
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+  this.intervalId = setInterval (this.update(),1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+  }
+  update() {
+    this.log();
+   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() {
+    if (this.floor <10) {
+        this.floor ++;
+        this.log();
+    }else {
+      console.log("Piso maximo alcanzado");
+    }
+
+
+}
+  floorDown() {
+    if (this.floor > 0) {
+        this.floor--;
+        this.log();
+    }else {
+      console.log("Piso minimo alcanzado");
+    }
+
+  }
+  call(person) {
+      this.requests.push(person.originFloor);
+      this.waitingList.push(person);
+
+   }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+   }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
 const Elevator = require('./elevator.js');
+
+const Person = require('./person.js');
+
+
+var elevator = new Elevator ();
+
+elevator.floorDown();
